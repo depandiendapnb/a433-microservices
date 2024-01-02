6b518e675aad3335e164e3e30ed825d2ccb1eff7
Depandi Enda <depandienda@gmail.com>
2024-01-02 17:12:14 +0700

    Karsajobs-ui submission-2

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker build -t depandienda22/karsajobs-ui:latest .
+
+echo $PASSWORD_DOCKER_HUB | docker login -u depandienda22 --password-stdin
+
+docker push depandienda22/karsajobs-ui:latest
